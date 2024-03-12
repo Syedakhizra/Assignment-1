b530d3560b95e62cfd354372864fa9de0d1f4da6
Syedakhizra <159184686+Syedakhizra@users.noreply.github.com>
2024-03-12 21:22:08 +0500

    Delete app.ts

--- app.ts
+++
@@ -1,9 +0,0 @@
-// lower case
-let personName: string ="khizra";
-console.log("lowercase:", personName.toLowerCase());
-
-// upper case
-console.log("uppercase:", personName.toLocaleUpperCase());
-
-// titile case
-console.log("titilecase:", personName.replace(/\bw/g,c => c.toUpperCase()));
