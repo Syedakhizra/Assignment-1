96dc9e79896c57765a2582bbb40c003735f6875c
Syedakhizra <159184686+Syedakhizra@users.noreply.github.com>
2024-03-12 21:21:58 +0500

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-// lower case
-var personName = "khizra";
-console.log("lowercase:", personName.toLowerCase());
-// upper case
-console.log("uppercase:", personName.toLocaleUpperCase());
-// titile case
-console.log("titilecase:", personName.replace(/\bw/g, function (c) { return c.toUpperCase(); }));
